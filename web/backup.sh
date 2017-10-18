de86cc5e147a9bdec20d310dfaed5753c91d43c9
Stone Chan <chenkang@tarenwang.com>
2017-10-18 11:48:48 +0800

    修改backup.sh

--- web/backup.sh
+++ web/backup.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-adirname() { odir=`pwd`; cd `dirname $1`; pwd; cd "${odir}"; }
-MYDIR=`adirname "$0"`
 DT=`date +%Y%m%d%H%M%S`
-EXCLUDEPATH=${MYDIR}/static
 tar -zcvf /home/bak/www.golang.mom_${DT}.tar.gz * --exclude=static
 echo "End"
